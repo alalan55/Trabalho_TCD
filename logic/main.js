1dc16bb16914474ba5a2b20bff803233d8670f51
aoliveira <aoliveira@keener.io>
2022-04-23 21:39:56 -0300

    Trabalho TCD

---
+++ logic/main.js
@@ -0,0 +1,44 @@
+
+if (window.Worker) {
+    const App = {
+        created() {
+            console.log('Matriz de dados com contem os valores:', this.dados)
+            this.somarDadosThread()
+        },
+        data() {
+            return {
+                dados: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                total: 0,
+                state: 0,
+                dadosParaThread: {}
+            }
+        },
+        watch: {
+            total(nv, ov) {
+                if (nv != ov) {
+                    this.somarDadosThread()
+                }
+            }
+        },
+        methods: {
+
+            somarDadosThread() {
+                if (this.state < this.dados.length) {
+
+                    this.dadosParaThread = { numeroAtual: this.dados[this.state], somaAtual: this.total }
+                    const worker = new Worker("/logic/thread.js");
+                    worker.postMessage(JSON.parse(JSON.stringify(this.dadosParaThread)))
+                    worker.onmessage = (result) => {
+                        this.total = result.data
+                    }
+
+                    this.state++
+
+                }
+
+            }
+        }
+    }
+    Vue.createApp(App).mount('#container')
+
+}
---
+++ logic/thread.js
@@ -0,0 +1,15 @@
+onmessage = function (data) {
+     console.log(`THREAD: dado recebido do main ${data.data.numeroAtual}`)
+     console.log('------------------------------------------------------')
+     console.log(`SOMA ATUAL NA TRHEAD: ${data.data.somaAtual}`)
+
+    var result = 0;
+
+    if (data.data.somaAtual == 0 || data.data.somaAtual > 0) {
+        result = data.data.somaAtual
+        result += data.data.numeroAtual
+    }
+
+
+    self.postMessage(result)
+}
